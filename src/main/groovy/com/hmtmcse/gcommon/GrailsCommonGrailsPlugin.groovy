3647da1338a0bab4fe9921c8ed04319a8ec432fa
Touhid Mia <hmtm.cse@gmail.com>
2019-09-05 22:55:59 +0600

    Migrate to grails plugin

---
+++ src/main/groovy/com/hmtmcse/gcommon/GrailsCommonGrailsPlugin.groovy
@@ -0,0 +1,20 @@
+package com.hmtmcse.gcommon
+
+
+import grails.plugins.Plugin
+
+
+class GrailsCommonGrailsPlugin extends Plugin {
+    def grailsVersion = "3.3.4 > *"
+    def pluginExcludes = [
+        "grails-app/views/error.gsp"
+    ]
+    def title = "Plugin"
+    def author = "Your name"
+    def authorEmail = ""
+    def description = '''\
+Brief summary/description of the plugin.
+'''
+    def profiles = ['web']
+    def documentation = "http://grails.org/plugin/plugin"
+}
