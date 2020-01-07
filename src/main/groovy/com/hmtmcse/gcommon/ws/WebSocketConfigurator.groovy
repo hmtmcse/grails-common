4cb76d9c1990f4e691e38c185d35bab7fe954ef8
Touhid Mia <touhid@bitmascot.com>
2020-01-07 16:06:35 +0600

    Grails Engine

--- src/main/groovy/com/hmtmcse/gcommon/ws/WebSocketConfigurator.groovy
+++ src/main/groovy/com/hmtmcse/gcommon/ws/WebSocketConfigurator.groovy
@@ -1,6 +1,5 @@
 package com.hmtmcse.gcommon.ws
 
-import javax.servlet.http.HttpSession
 import javax.websocket.HandshakeResponse
 import javax.websocket.server.HandshakeRequest
 import javax.websocket.server.ServerEndpointConfig
@@ -10,7 +9,6 @@ class WebSocketConfigurator extends ServerEndpointConfig.Configurator {
     @Override
     public void modifyHandshake(ServerEndpointConfig config, HandshakeRequest request, HandshakeResponse response) {
         super.modifyHandshake(config, request, response)
-//        config.getUserProperties().put("session", request.httpSession);
     }
 
 }
