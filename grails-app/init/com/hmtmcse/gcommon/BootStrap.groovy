e792c3faffb4d128405f4580bd8f3fc7753897ce
Touhid Mia <touhid@bitmascot.com>
2020-01-12 08:23:23 +0600

    Task

--- grails-app/init/com/hmtmcse/gcommon/BootStrap.groovy
+++ grails-app/init/com/hmtmcse/gcommon/BootStrap.groovy
@@ -1,6 +1,8 @@
 package com.hmtmcse.gcommon
 
 import com.hmtmcse.gcommon.ws.WebSocketHandler
+import grails.util.Holders
+import org.apache.catalina.util.ServerInfo
 
 class BootStrap {
 
@@ -8,8 +10,24 @@ class BootStrap {
 
     def init = { servletContext ->
        // WebSocketHandler.init(servletContext)
+        infoPrinter()
     }
 
     def destroy = {
+    }
+
+    public infoPrinter() {
+        println("Tomcat Information")
+        println("----------------------------------------------------------------------")
+        println("Server Info: ${ServerInfo.serverInfo}")
+        println("Server Number: ${ServerInfo.serverNumber}")
+        println("Server Built: ${ServerInfo.serverBuilt}")
+
+        println("\n")
+        println("Grails Plugin Information")
+        println("----------------------------------------------------------------------")
+        Holders.applicationContext.getBean("pluginManager").allPlugins.each{ plugin ->
+            println(plugin.name + " " + plugin.version)
+        }
     }
 }
